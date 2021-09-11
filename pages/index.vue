8fd0499602b647497e35e8e7447373746d5798f5
zischler <tobias.zischler@gmx.ch>
2021-09-11 21:41:22 +0200

    fix duplicate global components

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <svg-symbols></svg-symbols>
-    <vHeader></vHeader>
     <div class="my-6 p-4 rounded bg-gray-200 border w-1/3 text-center mx-auto">
       <h1 class="text-2xl">Localfarm - Basel Hack 2021</h1>
     </div>
