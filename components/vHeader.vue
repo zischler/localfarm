e280ec5c176bb9c3233319d3f5b23924208bd336
zischler <tobias.zischler@gmx.ch>
2021-09-11 14:28:58 +0200

    add header

---
+++ components/vHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header w-full h-16">
+    <div class="max-w-5xl w-full h-full mx-auto px-4">
+      <a href="/" class="w-full h-full flex items-center">
+        <svg
+          width="43"
+          height="38"
+          viewBox="0 0 43 38"
+          preserveAspectRatio="xMinYMax"
+        >
+          <use xlink:href="#logo"></use>
+        </svg>
+        <div class="ml-2">Localfarm</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<style>
+.header {
+  background-image: url("../static/header-bg.svg");
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="my-6 p-4 rounded bg-gray-200 border w-1/3 text-center mx-auto">
-    <h1 class="text-2xl">Localfarm - Basel Hack 2021</h1>
-  </div>
+    <div>
+        <svg-symbols></svg-symbols>
+        <vHeader></vHeader>
+        <div class="my-6 p-4 rounded bg-gray-200 border w-1/3 text-center mx-auto">
+          <h1 class="text-2xl">Localfarm - Basel Hack 2021</h1>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {}
+import VHeader from '~/components/vHeader.vue'
+import svgSymbols from '~/components/svgSymbols.vue'
+export default {
+  components: { VHeader, svgSymbols }
+}
 </script>
