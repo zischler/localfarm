97a0db0aa55572fe14ecb62a157f461ba5983995
Maciej Fiedler <maciekfiedler@gmail.com>
2021-09-11 18:19:53 +0200

    completed navbar and removed eslint

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,12 +1,59 @@
 <template>
-    <ul>
-        <li>Hello</li>
-        <li>Maps</li>
-        <li>Mehr</li>
+    <ul class="grid grid-cols-4 gap-2">
+        <li is="NavItem" v-for="obj,index of objs" :key="obj.text" :obj="obj" @clicked="changeToActive($event, index)"></li>
     </ul>
 </template>
 
 <script>
+import NavItem from '~/components/NavItem.vue'
+export default {
+    components: {NavItem},
+    data () {
+        return {
+            objs: [
+                {
+                    text: "Entdecken",
+                    path: '/',
+                    svg: 'compass',
+                    active: true
+                },
+                {
+                    text: "Karte",
+                    path: '/map',
+                    svg: 'map',
+                    active: false
+                },
+                {
+                    text: "Favoriten",
+                    path: '/favorites',
+                    svg: 'heart',
+                    active: false
+                },
+                {
+                    text: "Infos",
+                    path: '/info',
+                    svg: 'house',
+                    active: false
+                }
+            ]
+        }
+    },
+    methods: {
+        changeToActive (event, index) {
+            let obj = this.objs[index]
+            let objs = this.objs.filter(elem => elem != obj)
+            for(let i=0; i<objs.length; i++) {
+                let objInFor = objs[i]
+                objInFor.active = false
+            }
+            if(obj.text === event){
+                obj.active = true
+            } else {
+                obj.active = false
+            }
+        }
+    }
+}
 </script>
 
 <style>
---
+++ components/NavItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <li class="text-center">
+        <div @click="$emit('clicked', obj.text)">
+              <div>
+                <svg 
+                    class="mx-auto"
+                    width="43"
+                    height="38"
+                    viewBox="0 0 43 38"
+                    preserveAspectRatio="xMinYMax"
+                >
+                    <use v-bind:xlink:href="svgHref"></use>
+                </svg>
+              </div>
+              <p>{{obj.text}}</p>
+        </div>
+  </li>
+</template>
+
+<script>
+export default {
+    props: ['obj'],
+    computed: {
+        svgHref() {
+            let svg = "#" + this.obj.svg
+            if(this.obj.active) {
+                svg = svg + "-active"
+            }
+            return svg
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
