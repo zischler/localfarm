5c75dcb2870203597a3e4031f85dd66d31bc8028
Christian Huber <corethereal@gmail.com>
2021-09-11 12:51:15 +0200

    init base

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="my-6 p-4 rounded bg-gray-200 border w-1/3 text-center mx-auto">
+    <h1 class="text-2xl">Localfarm - Basel Hack 2021</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
