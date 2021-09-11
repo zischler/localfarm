ae52aeae45d2ffdb65d9b32a114bcf23d14648fa
Gianluca Frongia <gianluca.frongia@protonmail.com>
2021-09-11 20:21:28 +0200

    tailwind config color fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
     theme: {
-       'f-green': '#43AA8B',
-       'f-green-light': '#60F7CA',
-       'f-red': '#FF5470',
-       'f-red-light': '#FFA1B0',
+        colors: {
+            'f-green': '#43AA8B',
+            'f-green-light': '#60F7CA',
+            'f-red': '#FF5470',
+            'f-red-light': '#FFA1B0',
+        }
     }
   }
