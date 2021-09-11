67ce1ce2f8ac4943d3b6ba363e35a6c150410f45
Maciej Fiedler <maciekfiedler@gmail.com>
2021-09-11 16:02:33 +0200

    added global object where mockup api is stored and added uncomplete navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul>
+        <li>Hello</li>
+        <li>Maps</li>
+        <li>Mehr</li>
+    </ul>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,3 @@
-import VHeader from '~/components/vHeader.vue'
-import svgSymbols from '~/components/svgSymbols.vue'
-
 <template>
   <div>
     <svg-symbols></svg-symbols>
@@ -8,18 +5,32 @@ import svgSymbols from '~/components/svgSymbols.vue'
     <div class="my-6 p-4 rounded bg-gray-200 border w-1/3 text-center mx-auto">
       <h1 class="text-2xl">Localfarm - Basel Hack 2021</h1>
     </div>
+    <NavBar></NavBar>
   </div>
 </template>
 
 <script>
+import VHeader from '~/components/vHeader.vue'
+import svgSymbols from '~/components/svgSymbols.vue'
+import NavBar from '~/components/NavBar.vue'
 
 export default {
-  components: { VHeader, svgSymbols },
+  components: { VHeader, svgSymbols, NavBar },
+  data() {
+    return {
+      farmData: {}
+    }
+  },
+  async mounted () {
+    this.farmData = await this.fetchMockApi()
+  },
   methods: {
     async fetchMockApi () {
+      let result = {}
       await this.$axios.get(process.env.baseUrl)
-        .then(response => console.log(response))
+        .then(response => result = response)
         .catch(error => console.error(error))
+      return result
     }
   }
 }
