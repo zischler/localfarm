84e260e622d02500cf56daae9e11a386aa2d97c9
Christian Huber <corethereal@gmail.com>
2021-09-12 10:47:10 +0200

    bugfix: colors -> extend styling instead of overriding colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
     theme: {
-        colors: {
-            'f-green': '#43AA8B',
-            'f-green-light': '#60F7CA',
-            'f-red': '#FF5470',
-            'f-red-light': '#FFA1B0',
+        extend: {
+            colors: {
+                'f-green': '#43AA8B',
+                'f-green-light': '#60F7CA',
+                'f-red': '#FF5470',
+                'f-red-light': '#FFA1B0',
+            }
         }
     }
-  }
+}
