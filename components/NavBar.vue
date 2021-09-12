02764fc8f9269bdedece6c0c10d51c751769223a
zischler <tobias.zischler@gmx.ch>
2021-09-12 15:34:54 +0200

    add linkblock. change farm back location.

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="fixed bottom-0 bg-white w-full">
+    <div class="fixed bottom-0 bg-white w-full z-50">
         <ul class="grid grid-cols-4 gap-2 p-2 max-w-5xl mx-auto">
             <li is="NavItem" name="Entdecken" path="/" svg="entdecken"></li>
             <li is="NavItem" name="Karte" path="/map" svg="karte"></li>
